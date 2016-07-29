ab71503d75d7bc724d46f9245d538c6c28134a14
Adnan Ademovic <adnanademovic100@gmail.com>
2016-07-29 15:04:38 +0200

    Inline array getter and setter

--- serializer.js
+++ serializer.js
@@ -17,28 +17,23 @@
 
     [['Float32', 4], ['Int16', 2], ['Int8', 1], ['Uint8', 1], ['Uint16', 2]]
         .forEach(function(v) {
+            var key = v[0];
+            var step = v[1];
+
             byteRef.prototype['parse' + v[0] + 'Array'] = function(
                 view, destination) {
-                parseWith(this, view, 'get' + v[0], v[1], destination);
+                for (var i = 0; i < destination.length; i++) {
+                    destination[i] = view['get' + key](this.index, 1);
+                    this.add(step);
+                }
             };
 
             byteRef.prototype['set' + v[0] + 'Array'] = function(view,
                                                                  destination) {
-                setWith(this, view, 'set' + v[0], v[1], destination);
+                for (var i = 0; i < destination.length; i++) {
+                    view['set' + key](this.index, destination[i], 1);
+                    this.add(step);
+                }
             };
         });
-
-    function parseWith(parser, context, f, step, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            destination[i] = context[f](parser.index, 1);
-            parser.add(step);
-        }
-    }
-
-    function setWith(setter, context, f, step, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            context[f](setter.index, destination[i], 1);
-            setter.add(step);
-        }
-    }
 }());
