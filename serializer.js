b9ba4554c41faa7a7208415a3f2577893c92f04e
Adnan Ademovic <adnanademovic100@gmail.com>
2016-07-29 14:34:01 +0200

    Make serializer code less repetitive

--- serializer.js
+++ serializer.js
@@ -16,72 +16,56 @@
     };
 
     byteRef.prototype.parseFloat32Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            destination[i] = view.getFloat32(this.index, 1);
-            this.add(4);
-        }
+        parseWith(this, view, 'getFloat32', 4, destination);
     };
 
     byteRef.prototype.parseInt16Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            destination[i] = view.getInt16(this.index, 1);
-            this.add(2);
-        }
+        parseWith(this, view, 'getInt16', 2, destination);
     };
 
     byteRef.prototype.parseInt8Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            destination[i] = view.getInt8(this.index, 1);
-            this.add(1);
-        }
+        parseWith(this, view, 'getInt8', 1, destination);
     };
 
     byteRef.prototype.parseUint8Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            destination[i] = view.getUint8(this.index, 1);
-            this.add(1);
-        }
+        parseWith(this, view, 'getUint8', 1, destination);
     };
 
     byteRef.prototype.parseUint16Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            destination[i] = view.getUint16(this.index, 1);
-            this.add(2);
-        }
+        parseWith(this, view, 'getUint16', 2, destination);
     };
 
     byteRef.prototype.setFloat32Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            view.setFloat32(this.index, destination[i], 1);
-            this.add(4);
-        }
+        setWith(this, view, 'setFloat32', 4, destination);
     };
 
     byteRef.prototype.setInt8Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            view.setInt8(this.index, destination[i], 1);
-            this.add(1);
-        }
+        setWith(this, view, 'setInt8', 1, destination);
     };
 
     byteRef.prototype.setUint8Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            view.setUint8(this.index, destination[i], 1);
-            this.add(1);
-        }
+        setWith(this, view, 'setUint8', 1, destination);
     };
 
     byteRef.prototype.setUint16Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            view.setUint16(this.index, destination[i], 1);
-            this.add(2);
-        }
+        setWith(this, view, 'setUint16', 2, destination);
     };
 
     byteRef.prototype.setInt16Array = function(view, destination) {
-        for (var i = 0; i < destination.length; i++) {
-            view.setInt16(this.index, destination[i], 1);
-            this.add(2);
-        }
+        setWith(this, view, 'setInt16', 2, destination);
     };
+
+    function parseWith(parser, context, f, step, destination) {
+        for (var i = 0; i < destination.length; i++) {
+            destination[i] = context[f](parser.index, 1);
+            parser.add(step);
+        }
+    }
+
+    function setWith(setter, context, f, step, destination) {
+        for (var i = 0; i < destination.length; i++) {
+            context[f](setter.index, destination[i], 1);
+            setter.add(step);
+        }
+    }
 }());
