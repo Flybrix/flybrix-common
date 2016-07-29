036486461c0faa6578d1579e413337ab44500ce6
Adnan Ademovic <adnanademovic100@gmail.com>
2016-07-29 18:17:44 +0200

    Add coverage testing

--- karma.conf.js
+++ karma.conf.js
@@ -34,13 +34,17 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors:
         // https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            './cobs.js': ['coverage'],
+            './commandLog.js': ['coverage'],
+            './serializer.js': ['coverage'],
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['spec'],
+        reporters: ['spec', 'coverage'],
 
 
         // web server port
