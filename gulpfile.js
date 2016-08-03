d258a7ce348066747615afefb145b0e51d969bd4
Adnan Ademovic <adnanademovic100@gmail.com>
2016-08-03 18:08:07 +0200

    Add build automation using Gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('min', function() {
+    var files = [
+        'module.js',
+        'cobs.js',
+        'commandLog.js',
+        'serializer.js',
+        'parser.js',
+        'serial.js',
+        'deviceConfig.js',
+        'rcData.js',
+    ];
+    gulp.src(files)
+        .pipe(concat('flybrix-common.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
+});
