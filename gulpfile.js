3860dd93c39f098be93be78ab1b276574a077235
Adnan Ademovic <adnanademovic100@gmail.com>
2018-07-12 23:22:58 +0200

    Load descriptors from files

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var templateCache = require('gulp-angular-templatecache');
+var fs = require('fs');
 var packageInfo = require('./package.json');
+var descriptors = require('./descriptors/versions.json');
 
 var htmlFileList = ['src/**/*.html'];
 var jsFileList = ['src/**/*.js'];
-var libraryFiles = ['src/module.js', 'dist/templates.js'].concat(jsFileList);
+var libraryFiles = ['src/module.js', 'dist/descriptors.js', 'dist/templates.js'].concat(jsFileList);
 var watchedFileList = htmlFileList.concat(jsFileList);
 
 gulp.task('templates', function() {
@@ -20,7 +22,29 @@ gulp.task('templates', function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build-nomin', ['templates'], function() {
+gulp.task('descriptors', function() {
+    var files = {};
+    Object.keys(descriptors).forEach(function(key) {
+        var file = descriptors[key];
+        if (file in files) {
+            return;
+        }
+        files[file] = fs.readFileSync('./descriptors/' + file, 'utf8').replace(/\s/g,'');
+    });
+    var fileContents = "(function () {\n" +
+        "    'use strict';\n" +
+        "    descriptorsHandler.$inject = [];\n" +
+        "    angular.module('flybrixCommon').factory('descriptorsHandler', descriptorsHandler);\n" +
+        "    function descriptorsHandler() {\n" +
+        "        var versions = " + JSON.stringify(descriptors) + ";\n" +
+        "        var files = " + JSON.stringify(files) + ";\n" +
+        "        return { versions: versions, files: files };\n" +
+        "    }\n" +
+        "}());";
+    fs.writeFileSync('dist/descriptors.js', fileContents);
+});
+
+gulp.task('build-nomin', ['descriptors', 'templates'], function() {
     return gulp.src(libraryFiles)
         .pipe(plumber())
         .pipe(sourcemaps.init())
@@ -29,7 +53,7 @@ gulp.task('build-nomin', ['templates'], function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build-min', ['templates'], function() {
+gulp.task('build-min', ['descriptors', 'templates'], function() {
     return gulp.src(libraryFiles)
         .pipe(plumber())
         .pipe(sourcemaps.init())
